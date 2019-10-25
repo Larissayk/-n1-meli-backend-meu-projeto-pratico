58e41f8195e24ddb4ad510dcd1db2d44e59b06bb
Larissayk <lari.yukie@gmail.com>
2019-10-24 22:16:06 -0300

    ordenar tarefas por data

--- src/controller/tarefasController.js
+++ src/controller/tarefasController.js
@@ -41,22 +41,53 @@ exports.getByColaborador = (req, res) => {
     tarefa => tarefa.nomeColaborador.toLowerCase() == colaborador
   );
   if (resultado.length == 0) {
-    res.status(404).send(`Não foram encontradas tarefas para o colaborador ${colaborador}`);
-  }else{
+    res
+      .status(404)
+      .send(`Não foram encontradas tarefas para o colaborador ${colaborador}`);
+  } else {
     res.status(200).send(resultado);
-  } 
+  }
 };
 
 //Buscar Tarefas por Data de Inclusão e apresentar da mais nova para mais antiga
 exports.getByDataInclusao = (req, res) => {
- const datas = tarefas.map(tarefa => tarefa.dataInclusao);
- console.log(datas)
-// const datas2 = datas.map(dateString => dateString.split("/"))
-//  console.log(datas2);
- const datas3 = datas.map(
-   newDt => new Date(newDt) );
-  
- console.log(datas3);
+  // 01/20/2019
+//   const exData = "01/20/2019"
+//   const exDataFim = "01/25/2019"
+
+//   console.log(new Date(exData))
+//   const dataConv = new Date(exData)
+//   const dataConv2 = new Date(exDataFim)
+//   const data = new Date(exData).toISOString().split('T')[0]
+// //  const datas = [dataConv, dataConv2]
+// //  const sortDatas = datas.sort((a,b) => a-b)
+//  console.log(data)
+
+  // transformar string para data
+  // split - quebrar as datas
+  // data
+  // mes
+  // ano
+
+  const datas = tarefas.map(tarefa => tarefa.dataInclusao);
+  console.log(datas);
+  const datas2 = datas.map(dateString => new Date(dateString));
+  console.log(datas2);
+  datas2.forEach(OrdenarMaisNovo)
+  console.log(datas2)
+  const datasOrdenadas = datas2.map(data => data.toISOString().split('T')[0])
+  console.log(datasOrdenadas)
 
 
- }
+  function OrdenarMaisNovo (a, b) {
+    maisNovo = datas2.sort((a,b)=> a-b)
+  }
+
+  // function calcularDatas(dia,mes,ano) {
+  // const dataConvert = new Date()
+  // const dia = dataConvert.getDate()
+  // const mes = dataConvert.getMonth() +1
+  // const ano = dataConvert.getFullYear()
+  // console.log(dataConvert)
+  // }
+};
